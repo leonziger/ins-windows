385e7bc41aa33a53c8b901cd1c19faac479749d0
Leonid Zychkov <buldog52@gmail.com>
2019-01-26 15:02:26 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+window.$ = window.jQuery = require('jquery');
+import 'slick-carousel';
+
+import './components/main-header/main-header';
+import './components/services/services';
+import './components/menu-toggle/menu-toggle';
+import './components/modal/modal';
+import './components/callback/callback';
---
+++ src/components/services/services.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+$('.services').slick({
+    initialSlide:1,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
