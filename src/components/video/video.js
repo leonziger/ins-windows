8c865eeb644fdbaefbd7df2b6df38dd77cb173e5
Leonid Zychkov <buldog52@gmail.com>
2019-01-28 00:25:23 +0200

    Small refactoring in .video

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -16,6 +16,12 @@ slider.slick({
             settings: {
                 slidesToShow: 2
             }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
         }
     ]
 });
