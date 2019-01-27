b37a69f11244ea3a3eb8317353732fa28b2ffb5c
Leonid Zychkov <buldog52@gmail.com>
2019-01-27 13:35:30 +0200

    Added countdown timer to .action

--- src/components/action/action.js
+++ src/components/action/action.js
@@ -1,1 +1,10 @@
-// require('jquery-countdown');
+window.$ = window.jQuery = require('jquery');
+require('jquery-countdown');
+
+$('#getting-started').countdown('2019/02/09', function(event) {
+    // $(this).html(event.strftime('%D days %H:%M:%S'));
+    $('#clock-days').html(event.strftime('%D'));
+    $('#clock-hours').html(event.strftime('%H'));
+    $('#clock-minutes').html(event.strftime('%M'));
+    $('#clock-seconds').html(event.strftime('%S'));
+});
