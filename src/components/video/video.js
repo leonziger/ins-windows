73213e4111eeffc2936804f5b9a356bd7a3a81c2
Leonid Zychkov <buldog52@gmail.com>
2019-01-27 23:05:32 +0200

    code refactoring

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -3,13 +3,16 @@ import 'rangeslider.js';
 
 const slider = $('.video__list');
 slider.slick({
+    dots: false,
+    arrows:false,
     infinite: false,
     slidesToShow: 3,
     slidesToScroll: 1,
+    draggable: false,
 
     responsive: [
         {
-            breakpoint: 992,
+            breakpoint: 1200,
             settings: {
                 slidesToShow: 2
             }
@@ -21,7 +24,6 @@ slider.slick({
 $('#video-range').rangeslider({
     polyfill: false,
     onSlide: function(position, value) {
-        console.log(position, value);
         slider.slick('slickGoTo', value);
     }
 });
